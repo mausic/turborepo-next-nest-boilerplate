3c826dced883c0a67893e25d85ac8cf571137743
Maksym Ryndia <ryndya.maxim@gmail.com>
2025-03-03 18:50:31 +0200

    feat(web): introduce favicons config to app layout

--- apps/web/app/layout.tsx
+++ apps/web/app/layout.tsx
@@ -25,6 +25,12 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en" suppressHydrationWarning>
+      <link rel="apple-touch-icon" sizes="76x76" href="/static/favicons/apple-touch-icon.png" />
+      <link rel="icon" type="image/png" sizes="32x32" href="/static/favicons/favicon-32x32.png" />
+      <link rel="icon" type="image/png" sizes="16x16" href="/static/favicons/favicon-16x16.png" />
+      <link rel="shortcut icon" href="/static/favicons/favicon.ico" type="image/x-icon" />
+      <link rel="manifest" href="/static/favicons/site.webmanifest" />
+      <meta name="msapplication-config" content="/static/favicons/browserconfig.xml" />
       <body className={`${geistSans.variable} ${geistMono.variable}`}>
         <ThemeProvider
           attribute={"class"}
