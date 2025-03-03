9c2b8bc807d34ee914867cdb02f9adc32e95f81f
Maksym Ryndia <ryndya.maxim@gmail.com>
2025-03-03 17:00:22 +0200

    workspace(config): add .prettierrc.js config

---
+++ .prettierrc.js
@@ -0,0 +1,10 @@
+module.exports = {
+  printWidth: 100,
+  semi: true,
+  singleQuote: false,
+  bracketSpacing: true,
+  bracketSameLine: true,
+  trailingComma: "all",
+  arrowParens: "always",
+  tabWidth: 2,
+};
