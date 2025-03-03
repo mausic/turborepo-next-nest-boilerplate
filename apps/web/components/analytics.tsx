dbffcf37e5114af4e3c937f7620dac3395d22da0
Maksym Ryndia <ryndya.maxim@gmail.com>
2025-03-03 18:56:21 +0200

    feat(web): introduce google analytics

--- apps/web/components/analytics.tsx
+++ apps/web/components/analytics.tsx
@@ -1,3 +1,10 @@
+import { GoogleAnalytics } from "@next/third-parties/google";
+
 export const Analytics = () => {
-  return <div>{/* TODO: insert analytics related scripts here */}</div>;
+  return (
+    <>
+      {/* TODO: insert analytics related scripts here */}
+      <GoogleAnalytics gaId="G-XXXXXXXXXX" />
+    </>
+  );
 };
