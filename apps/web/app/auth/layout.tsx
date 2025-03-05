f4d0c50035200d46c9f12c9c9f5d28f8cf6a94f7
Maksym Ryndia <ryndya.maxim@gmail.com>
2025-03-05 22:09:43 +0200

    feat(web): add theme switcher to auth layout

--- apps/web/app/auth/layout.tsx
+++ apps/web/app/auth/layout.tsx
@@ -1,7 +1,15 @@
+import { ThemeSwitcher } from "@/components/theme-switcher";
 import * as React from "react";
 
 const AuthLayout = ({ children }: { children: React.ReactNode }) => {
-  return <div className="w-full h-screen flex items-center justify-center">{children}</div>;
+  return (
+    <div className="w-full h-screen flex flex-col">
+      <div className="pt-10 px-20 flex justify-end items-center w-full">
+        <ThemeSwitcher />
+      </div>
+      <div className="flex items-center justify-center w-full h-full">{children}</div>
+    </div>
+  );
 };
 
 export default AuthLayout;
