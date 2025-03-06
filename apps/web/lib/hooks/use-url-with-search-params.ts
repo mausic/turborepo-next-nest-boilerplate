a700407ded412d0590dbff084202dd9a806a1432
Maksym Ryndia <ryndya.maxim@gmail.com>
2025-03-06 16:02:46 +0200

    fix(web): remove `console.log`

--- apps/web/lib/hooks/use-url-with-search-params.ts
+++ apps/web/lib/hooks/use-url-with-search-params.ts
@@ -3,10 +3,8 @@ import { useSearchParams } from "next/navigation";
 
 export const useUrlWithSearchParams = () => {
   const searchParams = useSearchParams();
-  console.log(searchParams);
   const urlWithSearchParams = (url: string) => {
     const searchParamsString = new URLSearchParams(searchParams).toString();
-    console.log(searchParamsString);
     return `${url}?${searchParamsString}`;
   };
   return { urlWithSearchParams };
