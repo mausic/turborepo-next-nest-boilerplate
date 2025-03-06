9f8650467cc5aeaaf5470c2ea0c9a63a586d35b2
Maksym Ryndia <ryndya.maxim@gmail.com>
2025-03-06 20:25:56 +0200

    feat(eslint): add nestjs config files

---
+++ packages/eslint-config/prettier-base.js
@@ -0,0 +1,10 @@
+/** @type {import("prettier").Config} */
+module.exports = {
+  semi: true,
+  singleQuote: false,
+  bracketSpacing: true,
+  bracketSameLine: true,
+  trailingComma: "all",
+  arrowParens: "always",
+  tabWidth: 2,
+};
