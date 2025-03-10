67f36b824d2e73fb4585f4bae38e52b7d7f2a62f
Maksym Ryndia <ryndya.maxim@gmail.com>
2025-03-10 06:54:34 +0200

    feat(db): implements package building

--- packages/database/src/client.ts
+++ packages/database/src/client.ts
@@ -6,4 +6,4 @@ const globalForPrisma = global as unknown as { prisma: typeof prisma };
 
 if (process.env.NODE_ENV !== "production") globalForPrisma.prisma = prisma;
 
-export { prisma };
+export { prisma, PrismaClient };
